d218d46ca80544b231fcba11b5239f31d2ffd199
Dean.Wagman <deanwagman@gmail.com>
2014-06-12 00:03:41 -0400

    Added in comments

--- view.js
+++ view.js
@@ -1,31 +1,38 @@
+// View.js
+
+/* Setting the Canvas */
 var width = window.innerWidth - 30,
 	height = window.innerHeight - 30,
 	barWidth = width / model.values.length;
 
-var x = d3.scale.linear()
+var x, y, xAxis, yAxis, svg;
+
+x = d3.scale.linear()
 		.range([0,width])
 		.domain([1, d3.max(model.values)]);
 
-var y = d3.scale.linear()
+y = d3.scale.linear()
 		.range([height, 0])
 		.domain([0, d3.max(model.values)]);
 
-var xAxis = d3.svg.axis()
+xAxis = d3.svg.axis()
 		.scale(x)
 		.orient("bottom");
 
-var yAxis = d3.svg.axis()
+yAxis = d3.svg.axis()
     	.scale(y)
-    	.orient("left")
-    	.ticks(10, "%");
+    	.orient("left");
 
-var svg = d3.select("#container")
+svg = d3.select("#container")
     .attr("width", width)
     .attr("height", height)
    
+
+/* Main D3 function, updates view from model data */   
 function update(data, i, j){
 	var rect = svg.selectAll('rect').data(data);
 
+	/* Update */
 	rect.attr("y", function(d){ return y(d);})
 			.attr("height", function(d){ return height - y(d);})
 			.attr("class", function(d, index){
@@ -38,21 +45,14 @@ function update(data, i, j){
 				}
 			});
 
+	/* Remove */
 	rect.exit().remove();
 
+	/* Add */
 	rect.enter().append("rect")
 	  	.attr("x", function(d, i){ return i * barWidth;})
 		.attr("y", function(d){ return y(d);})
 		.attr("height", function(d){ return height - y(d);})
 		.attr("width", barWidth - 1)
 		.attr("class", "bar");
-
-	
-
 }
-
-
-
-model.randomize();
-update(model.values);
-model.bubbleSort();
