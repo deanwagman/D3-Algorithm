1b124647e2e555bab298bb88b1ee702b0a0b4689
Dean.Wagman <deanwagman@gmail.com>
2014-06-29 22:44:39 -0400

    Update timing function

--- model.js
+++ model.js
@@ -1,63 +1,69 @@
 /* Main Model */
 
 var model = {
-	values: [],
-	numBars: 100,
-	initialize: function(){
-		for(var i = 0;i < this.numBars; i++){
-			this.values.push(Math.floor(Math.random() * this.numBars));
-		}
-		this.randomize();
-	},
-	/* For future clock functionality */
-	time: function(){
-		return new Date();
-	},
-	/* Shuffles Array */
-	randomize: function(){
-		for(var i = 0; i < this.values.length; i++){
-			var j = Math.floor(Math.random() * this.values.length);
-			var jVal = this.values[j];
-			this.values[j] = this.values[i];
-			this.values[i] = jVal;
-		}
-	},
-	bubbleSort: function(){
-		var i = 0, id, sorted = false, j = model.values.length - 1;
-		var frame = setInterval(function(){
-			if(i < j){
-				if(model.values[i] > model.values[i+1]){
-					var temp = model.values[i];
-					model.values[i] = model.values[i+1];
-					model.values[i+1] = temp;
-				}
-				i++;
-			} 
-			else{
-				i = 0;
-				j--;
-				if(j == 0){
-					clearInterval(frame);
-				}
-			}
-			update(model.values, i, j);
-		}, 1000/60);
-	},
-	/* IN PROGRESS
-	mergeSort: function(){
-		var left =  0, 
-			right = this.values.length / 2;
-		var frame = setInterval(function(){
-			if(right < this.values.length){
-				if(this.values[right] < this.values[left]){
-					var temp = this.values[left];
-					this.values[left] = this.values[right];
-					this.values[right] = temp;
-				}
-			}
-		}, 1000/60);
-	} */
-	/* Todo: Add other Sorting algorithms */
+    values: [],
+    numBars: 120,
+    initialize: function () {
+        var i;
+        for (i = 0; i < this.numBars; i++) {
+            this.values.push(Math.floor(Math.random() * this.numBars));
+        }
+        this.randomize();
+    },
+    /* For future clock functionality */
+    time: function () {
+        return new Date();
+    },
+    /* Shuffles Array */
+    randomize: function () {
+        var i, j, jVal; // Left, Right, Temporary Value
+        for (i = 0; i < this.values.length; i++) {
+            j = Math.floor(Math.random() * this.values.length);
+            jVal = this.values[j];
+            this.values[j] = this.values[i];
+            this.values[i] = jVal;
+        }
+    },
+    bubbleSort: function () {
+        var i, j;
+        i = 0; // Left
+        j = model.values.length - 1; // Right
+        function step() {
+            var temp;
+            if (i < j) {
+                if (model.values[i] > model.values[i + 1]) {
+                    temp = model.values[i];
+                    model.values[i] = model.values[i + 1];
+                    model.values[i + 1] = temp;
+                }
+                i++;
+            } else {
+                i = 0;
+                j--;
+                if (j === 0) {
+                    update(model.values, i, j);
+                    return true;
+                }
+            }
+            update(model.values, i, j);
+        }
+        d3.timer(step, 1000);
+    },
+    /* IN PROGRESS
+    mergeSort: function(){
+        var left =  0, 
+            right = this.values.length / 2;
+        var frame = setInterval(function(){
+            if(right < this.values.length){
+                if(this.values[right] < this.values[left]){
+                    var temp = this.values[left];
+                    this.values[left] = this.values[right];
+                    this.values[right] = temp;
+                }
+            }
+        }, 1000/60);
+    } */
+    /* Todo: Add other Sorting algorithms */
 };
 
 model.initialize();
--- view.js
+++ view.js
@@ -1,58 +1,61 @@
 // View.js
 
+var model, view, d3;
+model = model || {};
+
+
 /* Setting the Canvas */
-var width = window.innerWidth - 30,
-	height = window.innerHeight - 30,
-	barWidth = width / model.values.length;
+var width = window.innerWidth - 40,
+    height = 300,
+    barWidth = width / model.values.length;
 
 var x, y, xAxis, yAxis, svg;
 
 x = d3.scale.linear()
-		.range([0,width])
-		.domain([1, d3.max(model.values)]);
+        .range([0, width])
+        .domain([1, d3.max(model.values)]);
 
 y = d3.scale.linear()
-		.range([height, 0])
-		.domain([0, d3.max(model.values)]);
+        .range([height, 0])
+        .domain([0, d3.max(model.values)]);
 
 xAxis = d3.svg.axis()
-		.scale(x)
-		.orient("bottom");
+        .scale(x)
+        .orient("bottom");
 
 yAxis = d3.svg.axis()
-    	.scale(y)
-    	.orient("left");
+        .scale(y)
+        .orient("left");
 
 svg = d3.select("#container")
     .attr("width", width)
-    .attr("height", height)
-   
+    .attr("height", height);
 
-/* Main D3 function, updates view from model data */   
-function update(data, i, j){
-	var rect = svg.selectAll('rect').data(data);
+/* Main D3 function, updates view from model data */
+function update(data, i, j) {
+    var rect = svg.selectAll('rect').data(data);
 
-	/* Update */
-	rect.attr("y", function(d){ return y(d);})
-			.attr("height", function(d){ return height - y(d);})
-			.attr("class", function(d, index){
-				if(index == i){
-					return "selector";
-				} else if(index > j){
-					return "done";
-				} else {
-					return "bar";
-				}
-			});
+    /* Update */
+    rect.attr("y", function (d) { return y(d); })
+        .attr("height", function (d) { return height - y(d); })
+        .attr("class", function (d, index) {
+            if (index === i) {
+                return "selector";
+            }
+            if (index > j) {
+                return "done";
+            }
+            return "bar";
+        });
 
-	/* Remove */
-	rect.exit().remove();
+    /* Remove */
+    rect.exit().remove();
 
-	/* Add */
-	rect.enter().append("rect")
-	  	.attr("x", function(d, i){ return i * barWidth;})
-		.attr("y", function(d){ return y(d);})
-		.attr("height", function(d){ return height - y(d);})
-		.attr("width", barWidth - 1)
-		.attr("class", "bar");
+    /* Add */
+    rect.enter().append("rect")
+        .attr("x", function (d, i) { return i * barWidth; })
+        .attr("y", function (d) { return y(d); })
+        .attr("height", function (d) { return height - y(d); })
+        .attr("width", barWidth - 1)
+        .attr("class", "bar");
 }
